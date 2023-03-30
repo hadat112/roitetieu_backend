e766742f387f430525f2c75feb685285e2ff7922
dathv33 <dathv33@ghtk.co>
2023-03-30 15:35:25 +0700

    add socket io

--- src/auth.js
+++ src/auth.js
@@ -18,77 +18,114 @@ import User from "./models/User";
 app.use(bodyParser.json());
 
 const updateRefreshToken = async (username, password, refreshToken) => {
-    const res = await User.findOneAndUpdate(
-        { user_name: username, password },
-        { $set: { refresh_token: refreshToken } }
-    );
+  User.findOneAndUpdate(
+    { user_name: username, password },
+    { $set: { refresh_token: refreshToken } }
+  );
 };
 
-app.post('/register', async (req, res) => {
-    const user_name = req.body.username;
-    const user = await User.findOne({ user_name: user_name });
-    if (user) res.status(200).send({ message: 'Tên tài khoản đã tồn tại.', success: false });
-    if (req.body.password !== req.body.confirm) res.status(200).send({ message: 'Hai mật khẩu bạn nhập không trùng nhau.', success: false });
-    else {
-        const hashPassword = bcrypt.hashSync(req.body.password, 10);
-        const hashPasswordConfirmation = bcrypt.hashSync(req.body.confirm, 10);
-        const accessToken = jwt.sign({ user_name }, 'secret', { expiresIn: '1500s' })
-        const refreshToken = jwt.sign({ user_name }, 'refreshsecret', { expiresIn: '30d' })
-        const newUser = new User({
-            user_name: user_name,
-            password: hashPassword,
-            passwordConfirmation: hashPasswordConfirmation,
-            refresh_token: refreshToken,
-        });
-        await newUser.save((error) => {
-            if (error) {
-                console.log(error);
-            } else {
-                res.json({ "success": true, data: { "token": accessToken, "refreshToken": refreshToken } });
-            }
-        });
-    }
-    return res.status(200).send({
-        data: username,
-        message: "Tao tk thanh cong",
-        success: true,
+app.post("/register", async (req, res) => {
+  const user_name = req.body.username;
+  const user = User.findOne({ user_name: user_name });
+  if (user)
+    res
+      .status(200)
+      .send({ message: "Tên tài khoản đã tồn tại.", success: false });
+  if (req.body.password !== req.body.confirm)
+    res
+      .status(200)
+      .send({
+        message: "Hai mật khẩu bạn nhập không trùng nhau.",
+        success: false,
+      });
+  else {
+    const hashPassword = bcrypt.hashSync(req.body.password, 10);
+    const hashPasswordConfirmation = bcrypt.hashSync(req.body.confirm, 10);
+    const accessToken = jwt.sign({ user_name }, "secret", {
+      expiresIn: "1500s",
     });
-}
-);
-
-app.post('/login', async (req, res) => {
-    const user_name = req.body.username;
-    const password = req.body.password;
-    const user = await User.findOne({ user_name: user_name })
-    if (!user) { return res.json({ "success": false, "message": "Tên đăng nhập không tồn tại." }) }
-    const isPasswordValid = bcrypt.compareSync(password, user.password);
-    if (!isPasswordValid) { return res.json({ "success": false, "message": "Mật khẩu không chính xác." }) }
-
-    const accessToken = jwt.sign({ user_name }, 'secret', { expiresIn: '1500s' })
-    const refreshToken = jwt.sign({ user_name }, 'refreshsecret', { expiresIn: '86400' })
-    updateRefreshToken(user_name, refreshToken);
-    res.json({ "success": true, data: { "token": accessToken, "refreshToken": refreshToken } });
+    const refreshToken = jwt.sign({ user_name }, "refreshsecret", {
+      expiresIn: "30d",
+    });
+    const newUser = new User({
+      user_name: user_name,
+      password: hashPassword,
+      passwordConfirmation: hashPasswordConfirmation,
+      refresh_token: refreshToken,
+    });
+    newUser.save((error) => {
+      if (error) {
+        console.log(error);
+      } else {
+        res.json({
+          success: true,
+          data: { token: accessToken, refreshToken: refreshToken },
+        });
+      }
+    });
+  }
+  return res.status(200).send({
+    data: username,
+    message: "Tao tk thanh cong",
+    success: true,
+  });
 });
 
-app.post('/refresh-token', async (req, res) => {
-    const refreshToken = req.body.refresh_token;
-    if (!refreshToken) return res.sendStatus(401).send({ success: false, message: 'Refresh token sai!' });
-    const user = await User.findOne({ refresh_token: refreshToken })
-    if (!user) return res.sendStatus(403);
-    const { user_name } = user;
-    try {
-        jwt.verify(refreshToken, 'refreshsecret')
-        const accessToken = jwt.sign({ user_name }, 'secret', { expiresIn: '1500s' })
-        const newRefreshToken = jwt.sign({ user_name }, 'refreshsecret', { expiresIn: '86400' })
-        updateRefreshToken(user_name, newRefreshToken)
+app.post("/login", async (req, res) => {
+  const user_name = req.body.username;
+  const password = req.body.password;
+  const user = User.findOne({ user_name: user_name });
+  if (!user) {
+    return res.json({
+      success: false,
+      message: "Tên đăng nhập không tồn tại.",
+    });
+  }
+  const isPasswordValid = bcrypt.compareSync(password, user.password);
+  if (!isPasswordValid) {
+    return res.json({ success: false, message: "Mật khẩu không chính xác." });
+  }
 
-        res.json({ "success": true, data: { "token": accessToken, "refreshToken": refreshToken } })
-    } catch (error) {
-        console.log(error)
-        res.sendStatus(403)
-    }
-})
+  const accessToken = jwt.sign({ user_name }, "secret", { expiresIn: "1500s" });
+  const refreshToken = jwt.sign({ user_name }, "refreshsecret", {
+    expiresIn: "86400",
+  });
+  updateRefreshToken(user_name, refreshToken);
+  res.json({
+    success: true,
+    data: { token: accessToken, refreshToken: refreshToken },
+  });
+});
+
+app.post("/refresh-token", async (req, res) => {
+  const refreshToken = req.body.refresh_token;
+  if (!refreshToken)
+    return res
+      .sendStatus(400)
+      .send({ success: false, message: "Refresh token sai!" });
+  const user = User.findOne({ refresh_token: refreshToken });
+  if (!user) return res.sendStatus(403);
+  const { user_name } = user;
+  try {
+    jwt.verify(refreshToken, "refreshsecret");
+    const accessToken = jwt.sign({ user_name }, "secret", {
+      expiresIn: "1500s",
+    });
+    const newRefreshToken = jwt.sign({ user_name }, "refreshsecret", {
+      expiresIn: "86400",
+    });
+    updateRefreshToken(user_name, newRefreshToken);
+
+    res.json({
+      success: true,
+      data: { token: accessToken, refreshToken: refreshToken },
+    });
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(403);
+  }
+});
 
 app.listen(PORT, () => {
-    console.log(`Example app listening on port ${PORT}`);
+  console.log(`Example app listening on port ${PORT}`);
 });
--- src/index.js
+++ src/index.js
@@ -1,29 +1,142 @@
-import express from 'express';
-import morgan from 'morgan';
-import path from 'path';
-import cors from 'cors';
-import bodyParser from 'body-parser';
-import route from './routes';
-import connect from './config/db';
-require('dotenv').config();
+import express from "express";
+import morgan from "morgan";
+import path from "path";
+import cors from "cors";
+import bodyParser from "body-parser";
+import route from "./routes";
+import connect from "./config/db";
+require("dotenv").config();
+const http = require("http");
+const socketio = require("socket.io");
+import createError from "http-errors";
 
-import createError from 'http-errors'
+const { addUser, removeUser, getUsersInRoom } = require("./controllers/users");
+const {
+  createGame,
+  addPlayer,
+  disconnectPlayer,
+  updatePlayerReadyStatus,
+  updatePlayerAnswer,
+  getGameByID,
+  gameLoop,
+} = require("./controllers/users");
 
 const app = express();
-const PORT = process.env.PUPPET_PORT || 4001;
+const server = http.createServer(app);
+const PORT = 5001;
 
 app.use(cors());
+route(app);
 
 // HTTP logger
-app.use(morgan('combined'));
-
+app.use(morgan("combined"));
 
 connect();
 
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 // method
 
-route(app);
+const io = socketio(server, {
+  cors: true,
+  origins: ["http://localhost:5173"],
+});
+
+console.log(io);
+
+// Socker Emitters
+const updateLeaderboard = (data, room) => {
+  io.to(room).emit("updateLeaderboard", {
+    leaderboard: data,
+  });
+};
+const updateGameState = (gameState, room) => {
+  io.to(room).emit("updateGameState", {
+    gameState,
+  });
+};
+
+io.on("connect", (socket) => {
+  console.log(`${socket.id} connected!`);
+
+  // Join room handler
+  socket.on("join", async ({ name, room }, callback) => {
+    const { error, user } = addUser({ id: socket.id, name, room });
+
+    if (error) return callback({ error: error });
+
+    socket.join(user.room);
+    const { game, error: getGameError } = getGameByID(room);
+
+    if (getGameError) {
+      await createGame(room);
+      addPlayer({ id: socket.id, name, room });
+    } else {
+      if (game.status !== "pending") {
+        return callback({ error: "Game already started" });
+      }
+      addPlayer({ id: socket.id, name, room });
+    }
+
+    io.to(user.room).emit("roomData", {
+      room: user.room,
+      users: getGameByID(user.room).game.players,
+    });
+
+    callback({
+      user: user,
+    });
+  });
+
+  // Set player ready status handler
+  socket.on("player-ready", ({ name, room }, callback) => {
+    const { game, error } = updatePlayerReadyStatus({
+      id: socket.id,
+      name,
+      room,
+    });
+
+    io.to(room).emit("roomData", {
+      room,
+      users: getGameByID(room).game.players,
+    });
+
+    if (game.status === "started") {
+      gameLoop({ room }, updateLeaderboard, updateGameState);
+    }
+    callback({ game, error });
+  });
+
+  // Set player's answer
+  socket.on(
+    "player-answer",
+    ({ name, room, questionID, answerID }, callback) => {
+      const { game, error } = updatePlayerAnswer({
+        id: socket.id,
+        name,
+        room,
+        questionID,
+        answerID,
+        momentAnswered: moment().format(),
+      });
+      callback({ game, error });
+    }
+  );
+
+  socket.on("disconnect", () => {
+    console.log(`${socket.id} disconnected.`);
+    const user = removeUser(socket.id);
+
+    if (user) {
+      const { error } = disconnectPlayer({ id: socket.id, room: user.room });
+      if (error !== "Game not found") {
+        io.to(user.room).emit("roomData", {
+          room: user.room,
+          users: getGameByID(user.room).game.players,
+        });
+      }
+    }
+  });
+});
 
 app.use((req, res, next) => {
   next(createError(404));
@@ -34,6 +147,6 @@ app.use((err, req, res) => {
   res.status(err.status || 500).send(err.message);
 });
 
-app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}`)
-})
+server.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}`);
+});
